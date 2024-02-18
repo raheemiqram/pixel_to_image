3c4f0c6eff8c451a534edb04eae3646989ab0f3c
raheem <raheem@kmmrce.com>
2024-02-18 09:23:48 +0400

    add: app url

--- apps/image_builder/api/urls.py
+++ apps/image_builder/api/urls.py
@@ -10,6 +10,11 @@ urlpatterns = [
         name="generate_image",
     ),
     path(
+        "original_image_list/",
+        views.OriginalImageList.as_view(),
+        name="original_image_list",
+    ),
+    path(
         "generated_image_list/<int:pk>/",
         views.GeneratedImageList.as_view(),
         name="generate_image-list",
