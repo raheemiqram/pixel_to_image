bfc8354e0b4e16689ef9cfbc4a96ddd3d31169e5
raheem <raheem@kmmrce.com>
2024-02-18 12:55:50 +0400

    WIP

--- apps/image_builder/api/serializers.py
+++ apps/image_builder/api/serializers.py
@@ -1,3 +1,5 @@
+import base64
+
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 from apps.image_builder.models import ReConstructImage, OriginalImage
@@ -13,16 +15,30 @@ class GenerateImageSerializer(serializers.Serializer):
 class GeneratedImageSerializer(ModelSerializer):
     width = serializers.SerializerMethodField()
     height = serializers.SerializerMethodField()
+    image_binary = serializers.SerializerMethodField()
 
     class Meta:
         model = ReConstructImage
-        fields = ("id", "reconstruct_image", "created_by", "created", 'width', 'height')
+        fields = ("id", "reconstruct_image", "created_by", "created", 'width', 'height', 'image_binary')
 
     def get_width(self, obj):
         return obj.reconstruct_image.width
 
     def get_height(self, obj):
         return obj.reconstruct_image.height
+
+    def get_image_binary(self, obj):
+        try:
+            # Open the image file
+            with open(obj.reconstruct_image.path, "rb") as img_file:
+                # Encode the binary image data in Base64
+                image_data_base64 = base64.b64encode(img_file.read())
+                return image_data_base64.decode("utf-8")  # Convert to UTF-8 for JSON serialization
+        except Exception as e:
+            # Handle any exceptions that may occur during image processing
+            print(f"Error processing image: {e}")
+
+        return None
 
 
 class OriginalImageSerializer(ModelSerializer):
